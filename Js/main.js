13c88b28112ce0ba1f17b7eee36e8e7017ae76cc
jonathanhrz25 <90871334+jonathanhrz25@users.noreply.github.com>
2021-11-30 18:57:05 -0600

    Versión 1

---
+++ Js/main.js
@@ -0,0 +1,31 @@
+let RegistrosHTML = document.getElementById('RegistrosHTML');
+
+let arrayRegistros = [];
+
+
+async function leeRegistros() {
+    const respuesta = await fetch("data/users.json");
+//    const respuesta = await fetch("http://veracano.com/jonathanhrz/index.php?nombre=Jonathan&apellido=Hernandez");
+    const registros = await respuesta.json();
+    
+    return registros;
+}
+async function muestraregistros() {
+    try{
+    arrayRegistros = await leeRegistros();
+    }
+    catch (e){
+        console.log("Error generado: "+e) 
+    }
+    console.log(arrayRegistros);
+    //RegistrosHTML.innerHTML=arrayRegistros;
+    //RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + arrayRegistros.nombre + " " + arrayRegistros.apellido;
+
+    arrayRegistros.forEach(function (item, index) {
+        RegistrosHTML.innerHTML = RegistrosHTML.innerHTML + "Nombre: " + item.nombre + " " + "=======> UserName del Jugador: " + item.userName +" ==== Numero de Telefono:" + item.telefono + "<br>";
+
+    });
+
+    
+}
+muestraregistros();
